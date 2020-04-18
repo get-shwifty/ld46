33f5495f9778157041797b5d9651145e43871e34
Félix <someonefefe@gmail.com>
2020-04-19 00:25:26 +0200

    adding fields assets

---
+++ src/assets/tilesets/fields.tsx
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="fields" tilewidth="501" tileheight="602" tilecount="19" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="500" height="400" source="../grounds/mountain_small.png"/>
+ </tile>
+ <tile id="1">
+  <image width="319" height="319" source="../grounds/sun.png"/>
+ </tile>
+ <tile id="2">
+  <image width="500" height="200" source="../grounds/ground_center_green.png"/>
+ </tile>
+ <tile id="3">
+  <image width="501" height="200" source="../grounds/ground_center_greener.png"/>
+ </tile>
+ <tile id="4">
+  <image width="501" height="200" source="../grounds/ground_center_red.png"/>
+ </tile>
+ <tile id="5">
+  <image width="501" height="200" source="../grounds/ground_center_yellow.png"/>
+ </tile>
+ <tile id="6">
+  <image width="500" height="200" source="../grounds/ground_left_green.png"/>
+ </tile>
+ <tile id="7">
+  <image width="501" height="200" source="../grounds/ground_left_greener.png"/>
+ </tile>
+ <tile id="8">
+  <image width="501" height="200" source="../grounds/ground_left_red.png"/>
+ </tile>
+ <tile id="9">
+  <image width="501" height="200" source="../grounds/ground_left_yellow.png"/>
+ </tile>
+ <tile id="10">
+  <image width="500" height="200" source="../grounds/ground_right_green.png"/>
+ </tile>
+ <tile id="11">
+  <image width="500" height="200" source="../grounds/ground_right_greener.png"/>
+ </tile>
+ <tile id="12">
+  <image width="501" height="200" source="../grounds/ground_right_red.png"/>
+ </tile>
+ <tile id="13">
+  <image width="501" height="200" source="../grounds/ground_right_yellow.png"/>
+ </tile>
+ <tile id="14">
+  <image width="500" height="200" source="../grounds/hole_green.png"/>
+ </tile>
+ <tile id="15">
+  <image width="500" height="200" source="../grounds/hole_greener.png"/>
+ </tile>
+ <tile id="16">
+  <image width="501" height="200" source="../grounds/hole_red.png"/>
+ </tile>
+ <tile id="17">
+  <image width="501" height="200" source="../grounds/hole_yellow.png"/>
+ </tile>
+ <tile id="18">
+  <image width="500" height="602" source="../grounds/mountain_big.png"/>
+ </tile>
+</tileset>
