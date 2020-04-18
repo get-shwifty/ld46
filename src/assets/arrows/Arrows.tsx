3e614f5657489d657d63f9af46ccb75f3ff93eaf
feldvale <12518162+feldvale@users.noreply.github.com>
2020-04-18 18:50:50 +0200

    Arrows tileset

---
+++ src/assets/arrows/Arrows.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="Arrows2" tilewidth="2400" tileheight="2400" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="1600" height="1600" source="arrow-down.png"/>
+ </tile>
+ <tile id="1">
+  <image width="2400" height="2400" source="arrow-left.png"/>
+ </tile>
+ <tile id="2">
+  <image width="634" height="256" source="arrow-right.png"/>
+ </tile>
+ <tile id="3">
+  <image width="1600" height="1600" source="arrow-up.png"/>
+ </tile>
+ <tile id="4">
+  <image width="1062" height="579" source="strong-arrow-right.png"/>
+ </tile>
+</tileset>
