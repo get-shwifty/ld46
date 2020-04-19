89dfc02700fdb3b87342278e5a76bc7d1435fe54
Félix <someonefefe@gmail.com>
2020-04-19 16:36:16 +0200

    cloud class working + rain particles

--- src/assets.js
+++ src/assets.js
@@ -4,10 +4,27 @@ import hole_1_green from './assets/grounds/hole_1_green.png';
 import hole_2_green from './assets/grounds/hole_2_green.png';
 import hole_3_green from './assets/grounds/hole_3_green.png';
 
+import cloud_1 from './assets/clouds/cloud_1.png';
+import cloud_2 from './assets/clouds/cloud_2.png';
+import cloud_3 from './assets/clouds/cloud_3.png';
+import cloud_4 from './assets/clouds/cloud_4.png';
+import cloud_5 from './assets/clouds/cloud_5.png';
+import cloud_6 from './assets/clouds/cloud_6.png';
+
+import water from './assets/particules/water.png'
+
 export function preload() {
     this.load.image('hole_0_green', hole_0_green);
     this.load.image('hole_1_green', hole_1_green);
     this.load.image('hole_2_green', hole_2_green);
     this.load.image('hole_3_green', hole_3_green);
-    
+
+    this.load.image('cloud_1', cloud_1);
+    this.load.image('cloud_2', cloud_2);
+    this.load.image('cloud_3', cloud_3);
+    this.load.image('cloud_4', cloud_4);
+    this.load.image('cloud_5', cloud_5);
+    this.load.image('cloud_6', cloud_6);
+   	
+   	this.load.image('water', water);
 }
---
+++ src/objects/cloud.js
@@ -0,0 +1,68 @@
+import Phaser from 'phaser';
+
+export default class Cloud extends Phaser.GameObjects.Container {
+
+	constructor(scene, x, y){
+
+		super(scene, x, y);
+		scene.add.existing(this);
+		
+		this.image = new Phaser.GameObjects.Image(this.scene, 0, 0, 'cloud');
+		this.add(this.image);
+
+		this.setData({
+			size: 1
+		});
+
+		this.on('changedata-size', this.onSizeChange, this);
+
+		const particles = scene.add.particles('water');
+		this.emitter = particles.createEmitter({
+			//x: { min: 200, max: 600 }, //Permet de faire tomber les gouttes sur une ligne horizontale
+			//y: 0,
+			angle: { min: 60, max: 120 }, 
+			speed: { min: 100, max: 300 },
+			frequency: 200,
+			lifespan: 8000,
+			quantity: 2,
+			scale: { start: 0.1, end: 0 }
+		});
+
+		this.emitter.startFollow(this);
+		this.stopRaining();
+	}
+
+	updateVue(size) { 
+		this.image.setTexture(`cloud_${size}`);
+	}
+
+	onSizeChange(){
+		console.log("oh my size changed!")
+		const size = this.getData('size');
+		this.updateVue(size);
+
+		if(size >= 3){
+			this.startRaining();
+		}else if(size <3){
+			this.stopRaining();
+		}
+	}
+
+	startRaining(){
+		this.emitter.start();
+	}
+
+	stopRaining(){
+		this.emitter.stop();
+	}
+
+	set size(newValue){
+		console.log("NewValue: " + newValue)
+		if(newValue > 0 && newValue <= 6){
+			this.setData({
+				size: newValue
+			});
+		}
+	}
+
+}
