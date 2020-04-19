49ddaaab82024ee664d8a5191a93b34a8fd90393
ddelmas <dfdelmas@gmail>
2020-04-19 19:27:04 +0200

    some ui tests

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import Hole from './objects/hole';
 const config = {
   type: Phaser.AUTO,
   parent: 'phaser-example',
-  width: 5000,
-  height: 2000,
+  width: 1500,
+  height: 1000,
   scene: {
     preload: preload,
     create: create
@@ -17,11 +17,15 @@ const config = {
 
 const game = new Phaser.Game(config);
 
+var menu = {};
+
 function preload() {
   preloadAssets.call(this);
 }
 
 function create() {
+  create_menu(this)
+
   this.cameras.main.setBackgroundColor('#ffffff');
   this.cameras.main.setBounds(0, 0, 4000, 1000);
 
@@ -31,4 +35,11 @@ function create() {
       water: Math.floor(Math.random() * 101)
     });
   }, 500);
+}
+
+function create_menu(game)
+{
+  menu.GameTitle = game.add.text(config.width /2 , 16, 'GAME TITLE', { fontSize: '32px', fill: '#000' });
+  menu.PlayButton = game.add.text(config.width /2, 60, 'PLAY', { fontSize: '20px', fill: '#000' });
+  menu.CreditsButton = game.add.text(config.width /2, 90, 'CREDITS', { fontSize: '20px', fill: '#000' });
 }
